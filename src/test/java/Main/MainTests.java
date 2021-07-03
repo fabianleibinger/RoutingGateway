f8bdb354b1de00f2e875b991164018057482d339
Fabian <fabian_leibinger@gmx.de>
2021-07-03 12:19:51 +0200

    folder and naming changes

--- src/test/java/Main/MainTests.java
+++ src/test/java/Main/MainTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package Main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MainTests {
 
 	@Test
 	void contextLoads() {
