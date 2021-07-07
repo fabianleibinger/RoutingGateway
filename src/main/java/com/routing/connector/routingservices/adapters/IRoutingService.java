928ef54a19273cded9048e63f981f7a7ae109ac1
Fabian <fabian_leibinger@gmx.de>
2021-07-07 19:14:18 +0200

    changed parameter of computeRoute() method to MobilityPreferences

--- src/main/java/com/routing/connector/routingservices/adapters/IRoutingService.java
+++ src/main/java/com/routing/connector/routingservices/adapters/IRoutingService.java
@@ -1,13 +1,13 @@
 package com.routing.connector.routingservices.adapters;
 
-import com.routing.connector.routingservices.requests.RoutingRequest;
+import com.routing.connector.models.MobilityPreferences;
 import com.routing.connector.routingservices.responses.RoutingResponse;
 
 import java.util.Optional;
 
 public interface IRoutingService {
 
-    RoutingResponse computeRoute(RoutingRequest request);
+    RoutingResponse computeRoute(MobilityPreferences preferences);
 
     String getName();
     String getURL();
