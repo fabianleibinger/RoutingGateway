3e76125616e87c5171ac044ce3162d28bb91c00d
Fabian <fabian_leibinger@gmx.de>
2021-07-22 17:03:48 +0200

    deleted unused imports

--- src/main/java/com/routing/gateway/preferenceservice/HttpPreferenceService.java
+++ src/main/java/com/routing/gateway/preferenceservice/HttpPreferenceService.java
@@ -6,8 +6,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
