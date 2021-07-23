85b2e5e0f39ad684f55075cda2017a694ba59fd6
Fabian <fabian_leibinger@gmx.de>
2021-07-23 11:28:00 +0200

    added OpenTripPlannerPlace

---
+++ src/main/java/com/routing/gateway/routingservices/responses/opentripplannerresponse/OpenTripPlannerAgencyAndId.java
@@ -0,0 +1,22 @@
+package com.routing.gateway.routingservices.responses.opentripplannerresponse;
+
+public class OpenTripPlannerAgencyAndId {
+    private String id;
+    private String agencyId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAgencyId() {
+        return agencyId;
+    }
+
+    public void setAgencyId(String agencyId) {
+        this.agencyId = agencyId;
+    }
+}
---
+++ src/main/java/com/routing/gateway/routingservices/responses/opentripplannerresponse/OpenTripPlannerTripPlan.java
@@ -0,0 +1,8 @@
+package com.routing.gateway.routingservices.responses.opentripplannerresponse;
+
+public class OpenTripPlannerTripPlan {
+    private Integer date;
+    private OpenTripPlannerPlace from;
+    private OpenTripPlannerPlace to;
+    private List<OpenTripPlannerItinerary> itineraries;
+}
