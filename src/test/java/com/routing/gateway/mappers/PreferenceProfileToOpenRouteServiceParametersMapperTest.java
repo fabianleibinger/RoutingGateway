38493933e3128afd951796221f96556bbdc4ccbb
Fabian <fabian_leibinger@gmx.de>
2021-07-29 14:59:06 +0200

    added JMapper and test, not working because of java 16

--- src/test/java/com/routing/gateway/mappers/PreferenceProfileToOpenRouteServiceParametersMapperTest.java
+++ src/test/java/com/routing/gateway/mappers/PreferenceProfileToOpenRouteServiceParametersMapperTest.java
@@ -2,19 +2,35 @@ package com.routing.gateway.mappers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.googlecode.jmapper.JMapper;
 import com.routing.gateway.preferenceservice.mobilitypreferences.PreferenceProfile;
 import com.routing.gateway.routingservices.requests.parameters.openrouteserviceparameters.OpenRouteServiceParameters;
 import fr.xebia.extras.selma.Selma;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.matchers.Null;
 
 public class PreferenceProfileToOpenRouteServiceParametersMapperTest {
+
     private PreferenceProfileToOpenRouteServiceParametersMapper mapper;
 
     @BeforeEach
     public void setUp() {
         mapper = Selma.builder(PreferenceProfileToOpenRouteServiceParametersMapper.class).build();
+    }
+
+    @Test
+    public void testJMapper() {
+        JMapper<OpenRouteServiceParameters, PreferenceProfile> jMapper;
+        jMapper = new JMapper<>(OpenRouteServiceParameters.class, PreferenceProfile.class);
+        /*jMapper = new JMapper<>(OpenRouteServiceParameters.class, PreferenceProfile.class,
+                "PreferenceProfileToOpenRouteServiceParametersMapper.xml");*/
+
+        PreferenceProfile profile = new PreferenceProfile();
+        profile.setProfileName("Jeff");
+        //profile.getWeighting().setComfort(75f);
+
+        OpenRouteServiceParameters parameters = jMapper.getDestination(profile);
+        assertEquals("Jeff", parameters.getPreference());
     }
 
     @Test
