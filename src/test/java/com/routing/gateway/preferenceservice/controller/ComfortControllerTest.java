8986ea7bff0087fe8349175acdefd82cda762e8f
Fabian <fabian_leibinger@gmx.de>
2021-07-31 08:48:15 +0200

    getUpdatedComfortFactors works fine

--- src/test/java/com/routing/gateway/preferenceservice/controller/ComfortControllerTest.java
+++ src/test/java/com/routing/gateway/preferenceservice/controller/ComfortControllerTest.java
@@ -1,4 +1,31 @@
 package com.routing.gateway.preferenceservice.controller;
 
+import com.routing.gateway.preferenceservice.User;
+import com.routing.gateway.preferenceservice.mobilitypreferences.ComfortFactors;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ComfortControllerTest {
+    private User user;
+    private String username = "maria.ziegler@online.de";
+    private String fullname = "Maria Ziegler";
+    private String password = "1234";
+
+    @BeforeEach
+    public void setup() {
+        user = new User();
+        user.setUsername(username);
+        user.setFullName(fullname);
+        user.setPassword(password);
+
+        user.login();
+    }
+
+    @Test
+    public void testGetUpdatedComfortFactors() {
+        ComfortFactors comfortFactors = ComfortController.getUpdatedComfortFactors(user);
+        for (String name : comfortFactors.getNames()) {
+            System.out.println(name);
+        }
+    }
 }
